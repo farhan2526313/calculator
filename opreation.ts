583e32a3e8cfb5501ccf7170825e96177faf7360
farhanbashir <farhan2526313@gmail.com>
2023-04-30 14:36:40 +0500

    a

---
+++ opreation.ts
@@ -0,0 +1,22 @@
+
+let addition = (x: number, y: number): number => {
+    return x + y;
+}
+
+let subtraction = (x: number, y: number): number => {
+    return x - y;
+}
+
+let multiplication = (x: number, y: number): number => {
+    return x * y;
+}
+
+let division = (x: number, y: number): number => {
+    return x / y;
+}
+
+export default (addition ;subtraction ; multiplication ; division);
+
+
+
+
